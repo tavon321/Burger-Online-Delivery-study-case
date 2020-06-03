079dd880b03406b707cfe65b754ca56a9090b22f
Gustavo Londono <gCorrea@buildingengines.com>
2020-06-03 09:12:30 -0500

    BurgerList: Create Burger Loader.

---
+++ BurgerList/BurgerList/BurgerLoader.swift
@@ -0,0 +1,13 @@
+//
+//  BurgerLoader.swift
+//  BurgerList
+//
+//  Created by Gustavo Londono on 6/3/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
+
+protocol BurgerLoader {
+    func load(completion: @escaping (Result<Burger, Error>) -> Void)
+}
