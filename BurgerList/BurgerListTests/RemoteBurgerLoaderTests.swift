4c696edad2fa28cd57f856fd3fc5d2b74defe4d3
Gustavo Londono <gCorrea@buildingengines.com>
2020-06-09 08:14:12 -0500

    Add Remote BurgerLaoder Tests

---
+++ BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteBurgerLoaderTests.swift
+//  BurgerListTests
+//
+//  Created by Gustavo Londono on 6/9/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
