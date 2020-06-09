fe814829ed26feb55677b06606ce09a186e713aa
Gustavo Londono <gCorrea@buildingengines.com>
2020-06-09 08:15:14 -0500

    BurgerList: RemoteBurgerLoader does not request data upon creation.

--- BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
+++ BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Gustavo Londono. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteBurgerLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromUrl() {
+        let client =  HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
