3b0893773dfe48ef7ab85c74ea00a5c0232f74aa
Gustavo Londono <gCorrea@buildingengines.com>
2020-06-10 09:15:32 -0500

    BurgerList: Guarantee that we can load multiple times.

--- BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
+++ BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
@@ -24,6 +24,15 @@ class RemoteBurgerLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwict_requestDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: Helpers
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteBurgerLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -34,8 +43,10 @@ class RemoteBurgerLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs: [URL] = []
         
         func get(form url: URL) {
+            requestedURLs.append(url)
             requestedURL = url
         }
     }
