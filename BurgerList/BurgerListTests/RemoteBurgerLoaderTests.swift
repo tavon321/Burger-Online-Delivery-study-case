2d8d1de85df133fe848d25c024b5044373110222
Gustavo Londono <gCorrea@buildingengines.com>
2020-06-10 08:43:12 -0500

    BurgerList: * Request Data from URL on `load`. * Move test logic to a Spy object. * Make the HTTPClient a protocol.

--- BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
+++ BurgerList/BurgerListTests/RemoteBurgerLoaderTests.swift
@@ -9,19 +9,44 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(httpClient: HTTPClient) {
+        client = httpClient
+    }
+    
+    func load() {
+        client.get(form: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(form url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(form url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteBurgerLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromUrl() {
-        let client =  HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(httpClient: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(httpClient: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
