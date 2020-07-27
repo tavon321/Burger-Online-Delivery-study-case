26d23bc5a9ee06488b81e5819a4a4b2d5d690961
Gustavo Londono <gCorrea@buildingengines.com>
2020-07-27 08:30:08 -0500

    Add result to a separated helper methos and track for possible leaks.

--- BurgerList/BurgerListAPIEndToEndTests/BurgerListAPIEndToEndTests.swift
+++ BurgerList/BurgerListAPIEndToEndTests/BurgerListAPIEndToEndTests.swift
@@ -12,10 +12,27 @@ import BurgerList
 class BurgerListAPIEndToEndTests: XCTestCase {
     
     func test_endToEndTestGetBurgerResult_matchesFixedTestAccountData() {
+        
+    }
+    
+    // Helpers
+    private func getBurgerResult() -> BurgerLoader.Result {
         let testServerURL = URL(string: "https://localhost:3000/burgerList")!
         let client = URLSessionHTTPClient()
         let loader = RemoteBurgerLoader(httpClient: client, url: testServerURL)
         
+        trackForMemoryLeaks(client)
+        trackForMemoryLeaks(loader)
+        
         let exp = expectation(description: "wait for load completion")
+        
+        var captureResult: BurgerLoader.Result?
+        loader.load { result in
+            captureResult = result
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        return captureResult
     }
 }
