ffd656890cc4e83ca6a9e9ea9a0a9076d8504e34
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-14 11:43:20 -0500

    Add `RemoteBurger` data transfer object representation to decouple the items mapper from `BurgerItem` model.

--- BurgerList/BurgerList/Burger API/BurgerMapper.swift
+++ BurgerList/BurgerList/Burger API/BurgerMapper.swift
@@ -8,26 +8,19 @@
 
 import Foundation
 
+struct RemoteBurger: Decodable {
+    let uuid: UUID
+    let name: String
+    let description: String?
+    let imageURL: URL?
+}
+
 internal final class BurgerMapper {
     
-    private struct RemoteBurger: Decodable {
-        let uuid: UUID
-        let name: String
-        let description: String?
-        let imageURL: URL?
-        
-        var burger: Burger {
-            return Burger(id: uuid,
-                          name: name,
-                          description: description,
-                          imageURL: imageURL)
-        }
-    }
-    
-    internal static func map(_ data: Data, response: HTTPURLResponse) -> RemoteBurgerLoader.Result {
+    internal static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteBurger] {
         guard response.isOK, let burgers = try? JSONDecoder().decode([RemoteBurger].self, from: data) else {
-            return .failure(RemoteBurgerLoader.Error.invalidData)
+            throw RemoteBurgerLoader.Error.invalidData
         }
-        return .success(burgers.map({ $0.burger }))
+        return burgers
     }
 }
