4420fda08830204aa430ab2370c4fd4731e6a477
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-14 11:46:56 -0500

    Refactor `LocalBurger` to his own file.

--- BurgerList/BurgerList/Burger Cache/BurgerStore.swift
+++ BurgerList/BurgerList/Burger Cache/BurgerStore.swift
@@ -15,17 +15,3 @@ public protocol BurgerStore {
     func deleteCacheFeed(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalBurger], timestamp: Date, completion: @escaping (Error?) -> Void)
 }
-
-public struct LocalBurger: Equatable {
-    public let id: UUID
-    public let name: String
-    public let description: String?
-    public let imageURL: URL?
-    
-    public init(id: UUID, name: String, description: String?, imageURL: URL?) {
-        self.id = id
-        self.name = name
-        self.description = description
-        self.imageURL = imageURL
-    }
-}
---
+++ BurgerList/BurgerList/Burger Cache/LocalBurger.swift
@@ -0,0 +1,23 @@
+//
+//  LocalBurger.swift
+//  BurgerList
+//
+//  Created by Gustavo Londono on 9/14/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
+
+public struct LocalBurger: Equatable {
+    public let id: UUID
+    public let name: String
+    public let description: String?
+    public let imageURL: URL?
+    
+    public init(id: UUID, name: String, description: String?, imageURL: URL?) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.imageURL = imageURL
+    }
+}
