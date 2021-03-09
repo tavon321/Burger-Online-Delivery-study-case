33724bc591fca2e0f23936b5ead92494d617e1d8
Gustavo Londono <tavon321@gmail.com>
2021-03-09 15:33:31 -0500

    Move `CodableBurgerStore` to production and demonstrate that we are following the liskov substitution principle

--- BurgerList/BurgerListTests/BurgerCache/Codable Burger Store/CodableBurgerStoreTests.swift
+++ BurgerList/BurgerListTests/BurgerCache/Codable Burger Store/CodableBurgerStoreTests.swift
@@ -9,82 +9,6 @@
 import XCTest
 import BurgerList
 
-class CodableBurgerStore: BurgerStore {
-
-    private struct Cache: Codable {
-        let burgers: [CodableLocalBurger]
-        let timestamp: Date
-
-        var localBuger: [LocalBurger] {
-            return burgers.map({ $0.local })
-        }
-    }
-
-    private struct CodableLocalBurger: Equatable, Codable {
-        public let id: UUID
-        public let name: String
-        public let description: String?
-        public let imageURL: URL?
-
-        init(_ local: LocalBurger) {
-            self.id = local.id
-            self.name = local.name
-            self.description = local.description
-            self.imageURL = local.imageURL
-        }
-
-        var local: LocalBurger {
-            LocalBurger(id: id, name: name, description: description, imageURL: imageURL)
-        }
-    }
-
-    private let storeUrl: URL
-
-    init(storeUrl: URL) {
-        self.storeUrl = storeUrl
-    }
-
-    func retrieve(completion: @escaping BurgerStore.RetreivalCompletion) {
-        guard let data = try? Data(contentsOf: storeUrl) else {
-            return completion(.success(nil))
-        }
-
-        do {
-            let decoder = JSONDecoder()
-            let cache = try decoder.decode(Cache.self, from: data)
-            let cachedBurgers = CachedBurgers(burgers: cache.localBuger, timestamp: cache.timestamp)
-            completion(.success(cachedBurgers))
-        } catch {
-            completion(.failure(error))
-        }
-    }
-
-    func insert(_ items: [LocalBurger], timestamp: Date, completion: @escaping BurgerStore.InsertionCompletion) {
-        do {
-            let encoder = JSONEncoder()
-            let cache = Cache(burgers: items.map(CodableLocalBurger.init), timestamp: timestamp)
-            let encoded = try encoder.encode(cache)
-
-            try encoded.write(to: storeUrl)
-            completion(nil)
-        } catch {
-            completion(error)
-        }
-    }
-
-    func deleteCacheFeed(completion: @escaping BurgerStore.DeletionCompletion) {
-        guard FileManager.default.fileExists(atPath: storeUrl.path) else {
-            return completion(nil)
-        }
-        do {
-            try FileManager.default.removeItem(at: storeUrl)
-            completion(nil)
-        } catch {
-            completion(error)
-        }
-    }
-}
-
 class CodableBurgerStoreTests: XCTestCase {
 
     override func setUp() {
@@ -205,29 +129,15 @@ class CodableBurgerStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Expected cached deletion fail")
     }
 
-    private func deleteCache(from sut: CodableBurgerStore) -> Error? {
-        let exp =  expectation(description: "Wait for deletion")
-
-        var deletionError: Error?
-        sut.deleteCacheFeed { receivedError in
-            deletionError = receivedError
-            exp.fulfill()
-        }
-
-        wait(for: [exp], timeout: 0.1)
-
-        return deletionError
-    }
-
     // MARK: - Helpers
-    private func makeSUT(url: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableBurgerStore {
+    private func makeSUT(url: URL? = nil, file: StaticString = #file, line: UInt = #line) -> BurgerStore {
         let sut = CodableBurgerStore(storeUrl: url ?? testStoreUrl())
 
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
 
-    private func expect(_ sut: CodableBurgerStore,
+    private func expect(_ sut: BurgerStore,
                         toRetrieve expectedResult: Result<CachedBurgers?, Error>,
                         file: StaticString = #file,
                         line: UInt = #line) {
@@ -249,8 +159,22 @@ class CodableBurgerStoreTests: XCTestCase {
         wait(for: [exp], timeout: 0.1)
     }
 
+    private func deleteCache(from sut: BurgerStore) -> Error? {
+        let exp =  expectation(description: "Wait for deletion")
+
+        var deletionError: Error?
+        sut.deleteCacheFeed { receivedError in
+            deletionError = receivedError
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 0.1)
+
+        return deletionError
+    }
+
     @discardableResult
-    private func insert(_ burgers: [LocalBurger], at timestamp: Date, to sut: CodableBurgerStore) -> Error? {
+    private func insert(_ burgers: [LocalBurger], at timestamp: Date, to sut: BurgerStore) -> Error? {
         let exp = expectation(description: "wait for cache insetion")
 
         var insertionError: Error?
@@ -263,7 +187,7 @@ class CodableBurgerStoreTests: XCTestCase {
         return insertionError
     }
 
-    private func expect(_ sut: CodableBurgerStore,
+    private func expect(_ sut: BurgerStore,
                         toRetrieveTwiceWith expectedResult: Result<CachedBurgers?, Error>,
                         file: StaticString = #file,
                         line: UInt = #line) {
