c422eb23af1cea8af90bdb773dd8a436995f4f4c
Gustavo Londono <tavon321@gmail.com>
2021-03-11 16:12:11 -0500

    Add comment telling the client that needs to handle the threats

--- BurgerList/BurgerList/Burger API/HTTPClient.swift
+++ BurgerList/BurgerList/Burger API/HTTPClient.swift
@@ -11,5 +11,9 @@ import Foundation
 public protocol HTTPClient {
     typealias Result = Swift.Result<(response: HTTPURLResponse, data: Data), Error>
     
+    /*
+     The completion handler can be invoked in any thread.
+     Clients are responsible to dispatch to appropiate threads, if needed.
+     */
     func get(from url: URL, completion: @escaping (Result) -> Void)
 }
