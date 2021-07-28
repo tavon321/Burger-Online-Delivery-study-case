fe45bf4c60b7e673ac5393a11817e690838a5143
Gustavo Londoño <tavon321@gmail.com>
2021-07-28 11:47:53 -0500

    add `BurgerListintegrationTests` to separate the slower cache integration tests from the isolated unit tests

---
+++ BurgerList/BurgerListCacheIntegrationTests/BurgerListCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  BurgerListCacheIntegrationTests.swift
+//  BurgerListCacheIntegrationTests
+//
+//  Created by Gustavo on 28/07/21.
+//  Copyright © 2021 Gustavo Londono. All rights reserved.
+//
+
+import XCTest
+
+class BurgerListCacheIntegrationTests: XCTestCase {
+
+}
