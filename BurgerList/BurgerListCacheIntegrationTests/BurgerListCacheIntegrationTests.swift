352b60703dd788bbe7e0803d1e321d973fab53ae
Gustavo Londoño <tavon321@gmail.com>
2021-07-30 08:06:31 -0500

    `load` delivers items saved on separated instances

--- BurgerList/BurgerListCacheIntegrationTests/BurgerListCacheIntegrationTests.swift
+++ BurgerList/BurgerListCacheIntegrationTests/BurgerListCacheIntegrationTests.swift
@@ -11,6 +11,18 @@ import BurgerList
 
 class BurgerListCacheIntegrationTests: XCTestCase {
     
+    override func tearDown() {
+        super.tearDown()
+        
+        setupEmptyState()
+    }
+    
+    override func setUp() {
+        super.setUp()
+        
+        undoStoreChanges()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -30,6 +42,32 @@ class BurgerListCacheIntegrationTests: XCTestCase {
         waitForExpectations(timeout: 0.1)
     }
     
+    func test_load_deliversItemsSavedOnSeparateInstance() {
+        let saveSut = makeSUT()
+        let loadSut = makeSUT()
+        let burgers = uniqueBurgers().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        saveSut.save(burgers) { saveError in
+            XCTAssertNil(saveError)
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 0.1)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        loadSut.load { result in
+            switch result {
+            case .success(let burgerList):
+                XCTAssertEqual(burgerList, burgers)
+            case .failure(let error):
+                XCTFail("Expected success, got \(error) instead")
+            }
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 0.1)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalBurgerLoader {
         let store = try! CoreDataBurgerStore(storeURL: testSpecificStoreURL())
@@ -39,6 +77,18 @@ class BurgerListCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return sut
+    }
+    
+    private func setupEmptyState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreChanges() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
