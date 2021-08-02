e158a0dc91a8ebc7533d6eb76a39c8eee8c3dd95
Gustavo Londoño <tavon321@gmail.com>
2021-08-02 07:51:56 -0500

    Add prototive folder to the app

---
+++ BurgerList-iOS/BurgerList-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerList-iOS
+//
+//  Created by Gustavo on 2/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
