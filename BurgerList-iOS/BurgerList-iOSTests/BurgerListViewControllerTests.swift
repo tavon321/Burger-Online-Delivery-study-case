8d251e38b7c08af50cf5ba0d82819af8c53dc83c
Gustavo Londoño <tavon321@gmail.com>
2021-08-06 08:59:13 -0500

    create `BurgerListViewControllerTests`

---
+++ BurgerList-iOS/BurgerList-iOSTests/BurgerListViewControllerTests.swift
@@ -0,0 +1,11 @@
+//
+//  BurgerListViewControllerTests.swift
+//  BurgerList-iOSTests
+//
+//  Created by Gustavo on 6/08/21.
+//
+
+import XCTest
+
+class BurgerListViewControllerTests: XCTestCase {
+}
