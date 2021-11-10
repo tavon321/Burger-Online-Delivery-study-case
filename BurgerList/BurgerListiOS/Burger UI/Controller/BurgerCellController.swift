d0e0c3d2ccedfbc0d37981f9f349b2a1385f8acd
Gustavo Londoño <tavon321@gmail.com>
2021-11-09 19:09:11 -0500

    Extract cell creaetion, configuration and image loading into a separate `BurgerCellController`

---
+++ BurgerList/BurgerListiOS/Burger UI/Controller/BurgerCellController.swift
@@ -0,0 +1,63 @@
+//
+//  BurgerCellController.swift
+//  BurgerListiOS
+//
+//  Created by Gustavo on 9/11/21.
+//  Copyright © 2021 Gustavo Londono. All rights reserved.
+//
+
+import UIKit
+import BurgerList
+
+final class BurgerCellController {
+    private var task:  BurgerImageDataLoadTask?
+    private let model: Burger
+    private let imageLoader: BurgerImageLoader
+    
+    init(model: Burger, imageLoader: BurgerImageLoader) {
+        self.model = model
+        self.imageLoader =  imageLoader
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = BurgerCell()
+        
+        cell.nameLabel.text = model.name
+        cell.descriptionLabel.text = model.description
+        cell.descriptionLabel.isHidden = model.description == nil
+        cell.burgerImageView.image = nil
+        cell.burgerImageRetryButton.isHidden = true
+        cell.imageContainer.startShimmering()
+        
+        let loadImage = { [weak self] in
+            guard let self = self, let url = self.model.imageURL else { return }
+            self.task = self.imageLoader.loadImageData(from: url) { [weak self] result in
+                    guard self != nil else { return }
+                    switch result {
+                    case .success(let imageData):
+                        let image = UIImage(data: imageData)
+                        
+                        cell.burgerImageView.image = image
+                        cell.burgerImageRetryButton.isHidden = image != nil
+                    case .failure:
+                        cell.burgerImageRetryButton.isHidden = false
+                    }
+                    cell.imageContainer.stopShimmering()
+                }
+        }
+        
+        loadImage()
+        cell.onRetry = loadImage
+        
+        return cell
+    }
+    
+    func preload() {
+        guard let url = self.model.imageURL else { return }
+        self.task = self.imageLoader.loadImageData(from: url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
--- BurgerList/BurgerListiOS/Burger UI/Controller/BurgerListViewController.swift
+++ BurgerList/BurgerListiOS/Burger UI/Controller/BurgerListViewController.swift
@@ -12,13 +12,14 @@ import BurgerList
 public final class BurgerListViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: BurgersRefreshViewController?
     private var imageLoader: BurgerImageLoader?
-    private var tasks = [IndexPath: BurgerImageDataLoadTask]()
     
     private var tableModel = [Burger]() {
         didSet {
             tableView.reloadData()
         }
     }
+    
+    private var cellControllers = [IndexPath: BurgerCellController]()
     
     public convenience init(burgerLoader: BurgerLoader,
                             imageLoader: BurgerImageLoader) {
@@ -47,63 +48,34 @@ public final class BurgerListViewController: UITableViewController, UITableViewD
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellModel = tableModel[indexPath.row]
-        let cell = BurgerCell()
-        
-        cell.nameLabel.text = cellModel.name
-        cell.descriptionLabel.text = cellModel.description
-        cell.descriptionLabel.isHidden = cellModel.description == nil
-        cell.burgerImageView.image = nil
-        cell.burgerImageRetryButton.isHidden = true
-        cell.imageContainer.startShimmering()
-        
-        let loadImage = { [weak self] in
-            guard let self = self, let url = cellModel.imageURL else { return }
-            self.tasks[indexPath] =
-                self.imageLoader?.loadImageData(from: url) { [weak self] result in
-                    guard self != nil else { return }
-                    switch result {
-                    case .success(let imageData):
-                        let image = UIImage(data: imageData)
-                        
-                        cell.burgerImageView.image = image
-                        cell.burgerImageRetryButton.isHidden = image != nil
-                    case .failure:
-                        cell.burgerImageRetryButton.isHidden = false
-                    }
-                    cell.imageContainer.stopShimmering()
-                }
-        }
-        
-        loadImage()
-        cell.onRetry = loadImage
-        
-        return cell
+        return cellController(forRowAt: indexPath).view()
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         guard tableModel[indexPath.row].imageURL != nil else { return }
-        cancelTask(forRowAt: indexPath)
+        cellControllers[indexPath] = nil
+        removeCellController(forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let cellModel = tableModel[indexPath.row]
-            
-            guard let imageURL = cellModel.imageURL else { return }
-            
-            tasks[indexPath] = imageLoader?.loadImageData(from: imageURL) { _ in }
+            cellController(forRowAt: indexPath).preload()
         }
     }
     
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-        indexPaths.forEach { indexPath in
-            cancelTask(forRowAt: indexPath)
-        }
+        indexPaths.forEach(removeCellController)
     }
     
-    private func cancelTask(forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+    private func removeCellController(forRowAt indexPath: IndexPath) {
+        cellControllers[indexPath] = nil
+    }
+    
+    private func cellController(forRowAt indexPath: IndexPath) -> BurgerCellController {
+        let cellModel = tableModel[indexPath.row]
+        let cellContoller = BurgerCellController(model: cellModel, imageLoader: imageLoader!)
+        
+        cellControllers[indexPath] = cellContoller
+        return cellContoller
     }
 }
