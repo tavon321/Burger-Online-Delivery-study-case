a785e56a85c8afe443ee456d937013226d9af676
Gustavo Londoño <tavon321@gmail.com>
2022-03-28 11:35:57 -0500

    `BurgerPresenter` message start loading view with `true` on `didStartLoadingBurgers`

--- BurgerList/BurgerListTests/BurgerPresentation/BurgerPresenterTests.swift
+++ BurgerList/BurgerListTests/BurgerPresentation/BurgerPresenterTests.swift
@@ -20,14 +20,25 @@ protocol BurgerErrorView {
     func display(_ viewModel: BurgerErrorViewModel)
 }
 
+struct BurgerLoadingViewModel {
+    var isLoading: Bool
+}
+
+protocol LoadingBurgerView: AnyObject {
+    func display(_ viewModel: BurgerLoadingViewModel)
+}
+
 final class BurgersPresenter {
     private let errorView: BurgerErrorView
+    private let loadingBurgerView: LoadingBurgerView
     
-    init(errorView: BurgerErrorView) {
+    init(errorView: BurgerErrorView, loadingBurgerView: LoadingBurgerView) {
         self.errorView = errorView
+        self.loadingBurgerView = loadingBurgerView
     }
     
     func didStartLoadingBurgers() {
+        loadingBurgerView.display(.init(isLoading: true))
         errorView.display(.noError)
     }
 }
@@ -39,12 +50,12 @@ class BurgerPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no messages on init, got \(view.messages) instead")
     }
     
-    func test_didStatLoadingBurger_displauNoErrorMessage() {
+    func test_didStatLoadingBurger_displauNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
         
         sut.didStartLoadingBurgers()
         
-        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none), .display(isLoading: true)])
     }
     
     // MARK: - Helpers
@@ -52,7 +63,7 @@ class BurgerPresenterTests: XCTestCase {
     private func makeSUT(file: StaticString = #file,
                          line: UInt = #line) -> (sut: BurgersPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = BurgersPresenter(errorView: view)
+        let sut = BurgersPresenter(errorView: view, loadingBurgerView: view)
         
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -60,15 +71,21 @@ class BurgerPresenterTests: XCTestCase {
         return (sut: sut, view: view)
     }
     
-    private class ViewSpy: BurgerErrorView {
-        private(set) var messages = [Message]()
+    private class ViewSpy: BurgerErrorView, LoadingBurgerView {
         
-        enum Message: Equatable {
+        private(set) var messages = Set<Message>()
+        
+        enum Message: Hashable {
             case display(errorMessage: String?)
+            case display(isLoading: Bool)
         }
         
         func display(_ viewModel: BurgerErrorViewModel) {
-            messages.append(.display(errorMessage: viewModel.errorMessage))
+            messages.insert(.display(errorMessage: viewModel.errorMessage))
+        }
+        
+        func display(_ viewModel: BurgerLoadingViewModel) {
+            messages.insert(.display(isLoading: viewModel.isLoading))
         }
     }
 }
